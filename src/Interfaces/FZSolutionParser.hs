03a3f62b5675985d440dbdec52eb28fa04fefa86
Maximilian Algehed <m.algehed@gmail.com>
2016-06-22 11:42:23 +0200

    Parser fixes

--- src/Interfaces/FZSolutionParser.hs
+++ src/Interfaces/FZSolutionParser.hs
@@ -67,7 +67,7 @@ makePairs :: [String] -> Solution
 makePairs []      = []
 makePairs ("Unsatisfiable":ls) = [("Unsatisfiable", "Unsatisfiable")]
 makePairs (l:ls)  = let (name, value) = fmap tail $ break ((==) '=') l
-                    in (init name, value) : makePairs ls
+                    in (name, value) : makePairs ls
 
 usefull :: [String] -> [String]
 usefull []                 = []
