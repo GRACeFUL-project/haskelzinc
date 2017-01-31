844f6c5c5c6d40d238f7449226b9fe1e1002fbb0
KlaraMar <klara.mar@cs.kuleuven.be>
2017-01-31 15:47:45 +0100

    MZASTBase documentation adapted

--- src/Interfaces/MZASTBase.hs
+++ src/Interfaces/MZASTBase.hs
@@ -7,12 +7,9 @@ License     : GPL-3
 Maintainer  : Klara Marntirosian <klara.mar@cs.kuleuven.be>
 Stability   : experimental
 
-This module provides an interface for the MiniZinc 2.1 language in Haskell through the definition of an abstract syntax tree of the MiniZinc language.
-With the use of this module, one can represent MiniZinc models in Haskell code. The abstract syntax tree is based on 
-<http://www.minizinc.org/doc-lib/minizinc-spec.pdf the MiniZinc 2.1 spesification>. 
-
-However, the module does not check semantical correctness of the represented model.
-For example, it does not detect typos in the use of previously declared identifiers.
+This module provides an interface for the MiniZinc 2.1 language.
+With the use of this module, one can represent MiniZinc models in Haskell code. The syntax is based on 
+<http://www.minizinc.org/doc-lib/minizinc-spec.pdf the MiniZinc 2.1 spesification>.
 -}
 
 module Interfaces.MZASTBase (
@@ -40,13 +37,16 @@ module Interfaces.MZASTBase (
 -- | An abbreviation for the type of a represented MiniZinc model.
 type MZModel = [Item]
 
+-- | Depresents MiniZinc items, the first-class entities of the MiniZinc language.
+-- Variable, function, predicate, test and annotation declaration items are all 
+-- represented with the @Declare@ constructor.
 data Item 
   -- | Commented line
   = Comment String
   -- | Include item
   | Include Filename
-  -- | A declaration item. Can represent a MiniZinc variable, predicate, test, function or 
-  -- annotation declaration. This is specified by the constructor's argument.
+  -- | A declaration item. Can represent a MiniZinc variable, predicate, test, function 
+  -- or annotation declaration. This is specified by the constructor's argument.
   | Declare Declaration
   -- | Assignment item. @Assign name exp@ represents the assignment of @exp@ to the variable @name@.
   | Assign Ident AnnExpr
@@ -54,20 +54,24 @@ data Item
   | Constraint AnnExpr
   -- | Solve item
   | Solve Solve
-  -- | Output item. The use of this item might cause errors in parsing the solution(s) of the model.
-  -- Recommended use for testing purposes only.
+  -- | Output item. The use of this item might cause errors in parsing the solution(s) 
+  -- of the model. Recommended use for testing purposes only.
   | Output Expr
-  -- | Represents an empty line in the MiniZinc model.
+  -- | @Empty@ does not translate to a MiniZinc item. It only represents an empty line 
+  -- in the MiniZinc code.
   | Empty        
   deriving (Show, Eq)
 
--- Represents a MiniZinc expression (first argument) annotated with the annotations contained in the list of the second argument.
+-- | Represents a MiniZinc expression (first argument) annotated with the annotations 
+-- contained in the list of the second argument.
 data AnnExpr = AnnExpr Expr [Annotation]
   deriving (Show, Eq)
 
+-- | Transforms a @Expr@ to an @AnnExpr@ with an empty list of annotations.
 toSimpleExpr :: Expr -> AnnExpr
 toSimpleExpr e = AnnExpr e []
 
+-- | Takes a annotated expression and returns only the expression.
 stripExprOff :: AnnExpr -> Expr
 stripExprOff (AnnExpr e ans) = e
 
@@ -82,8 +86,8 @@ class Foldable t => DeclarationG (t a) where
   type DS (t a)
   (=.) ::  -}
 
--- | Specifies wether the declaration is a variable, predicate, test, function or annotation
--- declaration. For each case, the arguments hold the signature of the declaration.
+-- | Builds the representation of the signature of a variable, function, predicate, test
+-- or annotation declaration.
 data DeclarationSignature = Variable Param 
                           | Predicate Ident [Param]
                           | Test Ident [Param]
@@ -105,35 +109,43 @@ data Expr
   | FConst Float
   -- | MiniZinc string value
   | SConst String
-  -- | @SetLit literals@ translates to a MiniZinc set the elements of which are the represented expressions in
-  -- the @literals@ list.
+  -- | @SetLit literals@ translates to a MiniZinc set the elements of which are the 
+  -- represented expressions in the @literals@ list.
   | SetLit [Expr]
-  -- | MiniZinc set comprehension. The first argument of the constructor represents the head
-  -- expression of the comprehension, while the second represents the comprehension tail.
+  -- | MiniZinc set comprehension. The first argument of the constructor represents the
+  -- head expression of the comprehension, while the second represents the comprehension 
+  -- tail.
   | SetComp Expr CompTail
-  -- | MiniZinc 1-dimensional arrays defined with literals, similar to the @SetLit@ constructor.
+  -- | MiniZinc 1-dimensional arrays defined with literals, similar to the @SetLit@ 
+  -- constructor.
   | ArrayLit [Expr]
   -- | MiniZinc 2-dimensional arrays defined with literals
   | ArrayLit2D [[Expr]]
   -- | MiniZinc array comprehension. Syntax similar to @SetComp@ constructor.
   | ArrayComp Expr CompTail
-  -- | Represents an array element. In @ArrayElem name is@, the argument @name@ is the identifier of the array and @is@ is
-  -- the list of indexes that specify the desired element. The length of @is@ must be equal to the number of 
-  -- dimensions of the array.
+  -- | Represents an array element. In @ArrayElem name is@, the argument @name@ is the 
+  -- identifier of the array and @is@ is the list of indexes that specify the desired 
+  -- element. The length of @is@ must be equal to the number of dimensions of the array.
   | ArrayElem Ident [Expr]
-  -- | @Bi op exp1 exp2@ represents the MiniZinc expression that applies the binary operator @op@ on @exp1@ and @exp2@.
+  -- | @Bi op exp1 exp2@ represents the MiniZinc expression that applies the binary 
+  -- operator @op@ on @exp1@ and @exp2@.
   | Bi Op Expr Expr
-  -- | @U op exp1@ represents the MiniZinc expression that applies the unary operator @op@ on @exp1@.
+  -- | @U op exp1@ represents the MiniZinc expression that applies the unary operator 
+  -- @op@ on @exp1@.
   | U Op Expr
-  -- | @Call name args@ represents a call to the function or test @name@ on arguments @args@.
+  -- | @Call name args@ represents a call to the function, predicate or test @name@ on 
+  -- arguments @args@. A call to an annotation is represented by the @Annotation@ type.
   | Call Ident [AnnExpr]
-  -- | The if-then-else conditional. If the first argument of the constructor is an empty list, the translation to MiniZinc will fail.
-  -- @ITE [(cond, expr1)] expr2@, where the list is a singleton, translates to @if cond then exp1 else exp2 endif@.
-  -- If the list contains more than one pairs, then the corresponding @elseif-then@ pairs are inserted before the final @else@ expression.
+  -- | The if-then-else conditional. If the first argument of the constructor is an 
+  -- empty list, the translation to MiniZinc will fail. @ITE [(cond, expr1)] expr2@,
+  -- where the list is a singleton, translates to @if cond then exp1 else exp2 endif@.
+  -- If the list contains more than one pairs, then the corresponding @elseif-then@
+  -- pairs are inserted before the final @else@ expression.
   | ITE [(Expr, Expr)] Expr
-  -- | @let-in@ expression. In @Let items expr@, the elements of @items@ represent the bindings in the @expr@ expression. Although @items@
-  -- is of type @[Items]@, only @Item@ values constructed by @Declare@ and @Constraint@ will translate to a syntactically correct
-  -- MiniZinc let expression.
+  -- | @let-in@ expression. In @Let items expr@, the elements of @items@ represent the 
+  -- bindings in the @expr@ expression. Although @items@ is of type @[Item]@, only 
+  -- @Item@ values constructed by @Declare@ and @Constraint@ will translate to a 
+  -- syntactically correct MiniZinc let expression.
   | Let [Item] Expr
   -- | A generator call expression.
   | GenCall Ident CompTail Expr 
@@ -165,18 +177,23 @@ data Type
   | VarType Ident
   deriving (Show, Eq)
 
--- | Represents an operator name in MiniZinc.
+-- | Represents an operator name/symbol in MiniZinc.
 newtype Op = Op Ident
   deriving (Show, Eq)
 
+-- | Transforms an operator to a quoted operator (which, in MiniZinc admits prefix 
+-- notation). One can represent the application of a quoted operator though the @Call@
+-- constructor of type @Item@.
 prefixOp :: Op -> Ident
 prefixOp (Op op) = "`" ++ op ++ "`"
 
+-- | Used in annotations' arguments, which can be either annotations or expressions.
 data GArguments = A Annotation
-               | E Expr
+                | E Expr
   deriving (Show, Eq)
 
--- | Represents a call to a MiniZinc annotation.  
+-- | Represents a call to a MiniZinc annotation. First argument represents the 
+-- annotation's name and second argument contains the annotation's arguments, if any.
 data Annotation = Annotation Ident [GArguments]
   deriving (Show, Eq)
 
