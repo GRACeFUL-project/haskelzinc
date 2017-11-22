d59495b9684ec0bcf323d40a3db7e9edbef99db6
Gert-Jan Bottu <gertjanbottu@hotmail.com>
2017-11-22 11:28:46 +0100

    Working example for AS constraints

--- ASExamples.hs
+++ ASExamples.hs
@@ -1,14 +1,16 @@
 module ASExamples where
 
 import Interfaces.MZBuiltIns
-import Interfaces.MZAST
 import Interfaces.MZinHaskell
+import Interfaces.MZAST
+import TimeSpaceConstr.ActionSequences
+import TimeSpaceConstr.TimeSpaceConstr
 
 main = iRunModel example1
 
 example1 = [
   include "regular.mzn",
-  var (Array [CT $ 1..20] Dec (CT $ 1..4)) "x",
-  actionSequence 2 (atmost 1 3) (Var (Simpl "x"))
+  var (Array [CT $ 1...20] Dec (CT $ 1...4)) "x",
+  constraint $ actionSequence 2 (atmost 1 3) (Var (Simpl "x")),
   solve satisfy
            ]
