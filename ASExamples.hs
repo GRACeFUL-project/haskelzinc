e7ccd5669195d87ff57d883e8fd7b1d0ec3509fd
Gert-Jan Bottu <gertjanbottu@hotmail.com>
2017-11-30 12:46:53 +0100

    TimeSpaceCtr example from slides

--- ASExamples.hs
+++ ASExamples.hs
@@ -6,11 +6,23 @@ import Interfaces.MZAST
 import TimeSpaceConstr.ActionSequences
 import TimeSpaceConstr.TimeSpaceConstr
 
-main = iRunModel example1
+main = iRunModel example2
 
 example1 = [
   include "regular.mzn",
   var (Array [CT $ 1...20] Dec (CT $ 1...4)) "x",
   actionSequence 2 (atmost 1 3) (Var (Simpl "x")),
+  solve satisfy
+           ]
+
+example2 = [
+  include "regular.mzn",
+  var (Array [CT $ 1...10] Dec (CT $ 1...8)) "x",
+  actionSequence 6 (atleast 1 1) (Var (Simpl "x")),
+  actionSequence 6 (or_as 2 3) (Var (Simpl "x")),
+  actionSequence 6 (or_as 3 4) (Var (Simpl "x")),
+  actionSequence 6 (or_as 5 6) (Var (Simpl "x")),
+  actionSequence 6 (implication 2 6) (Var (Simpl "x")),
+  actionSequence 6 (value_precedence 3 5) (Var (Simpl "x")),
   solve satisfy
            ]
